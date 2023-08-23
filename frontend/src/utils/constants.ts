b5d20bc5b3b89d233fd691cd38bb55840adad66e
Mystical94 <53875398+Mystical94@users.noreply.github.com>
2023-08-23 15:58:28 +0530

    frontend
    taken inspiration from https://github.com/vden/hazed

---
+++ frontend/src/utils/constants.ts
@@ -0,0 +1,11 @@
+import xdcLogo from '../svg/xdc-logo.png';
+import { AddrMapType, MapType } from './types';
+
+export const networkIcons: MapType = {
+  51: xdcLogo
+}
+
+export const registryAddress: AddrMapType = {
+  0: '0x0',
+  51: '0x7153B4cf7cf35142F4aB175D29A7d45a734db679'
+}
