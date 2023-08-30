c6d0bcaaf52f2f02ba91f04c959c37b7c6d34ac8
Mystical94 <53875398+Mystical94@users.noreply.github.com>
2023-08-30 17:01:22 +0530

    fixes

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -1,16 +1,13 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
-
-contract Registry {
+contract Xcrypt {
   using SafeERC20 for IERC20;
 
-  struct EphemeralKey {
+  struct senderPublicKey {
     bytes32 x;
     bytes32 y;
     bytes1 ss;
@@ -22,42 +19,42 @@ contract Registry {
   uint8 public constant VERSION = 1;
 
   address private owner;
-  EphemeralKey[] keys;
+  senderPublicKey[] keys;
 
   constructor() {
     owner = msg.sender;
   }
 
-  function publish(bytes32 x, bytes32 y, bytes1 ss, address token) private {
-    keys.push(EphemeralKey(x, y, ss, token));
+  function send(bytes32 x, bytes32 y, bytes1 ss, address token) private {
+    keys.push(senderPublicKey(x, y, ss, token));
   }
 
   function totalKeys() view external returns (uint256 count) {
     count = keys.length;
   }
 
-  function publishAndSendToken(bytes32 x, bytes32 y, bytes1 ss, address token, address target, uint256 amount) external {
-    require(amount > 0, "R: Sending amount should be more than 0");
-    require(token != address(0x0), "R: Token contract required");
-    require(target != address(0x0), "R: Target address required");
+  function sendTokenAnonymously(bytes32 x, bytes32 y, bytes1 ss, address token, address target, uint256 amount) external {
+    require(amount > 0, "Sending amount should be greater than 0");
+    require(token != address(0x0), "Token contract address cannot be Zero");
+    require(target != address(0x0), "Receiver address cannot be Zero");
 
-    publish(x, y, ss, token);
+    send(x, y, ss, token);
 
     IERC20(token).safeTransferFrom(msg.sender, target, amount);
   }
 
-  function publishAndSend(bytes32 x, bytes32 y, bytes1 ss, address payable target) public payable {
+  function sendAnonymously(bytes32 x, bytes32 y, bytes1 ss, address payable target) public payable {
     require(msg.value > 0, "R: Sending amount should be more than 0");
     require(target != address(0x0), "R: Target address required");
 
-    publish(x, y, ss, XDC);
+    send(x, y, ss, XDC);
 
     (bool sent, ) = target.call{value: msg.value}("");
     require(sent, "R: Failed to send XDC");
   }
 
-  function getNextKeys(uint256 start) external view returns (EphemeralKey[10] memory) {
-    EphemeralKey[10] memory gotKeys;
+  function getNextKeys(uint256 start) external view returns (senderPublicKey[10] memory) {
+    senderPublicKey[10] memory gotKeys;
 
     uint256 end = start + 10;
     uint256 limit = (keys.length < end) ? keys.length : end;
